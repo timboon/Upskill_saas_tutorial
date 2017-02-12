509755de0a2b987231b551b4cb780dbddc590f5a
Oliver Wasse <o.wasse@iinet.net.au>
2017-02-12 08:53:20 +0000

    added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,71 +1,58 @@
-/* global $,Stripe */
-
+/* global $, Stripe */
 //Document ready.
 $(document).on('turbolinks:load', function(){
-  var theForm = $('#pro_form');    
-  var submitBtn = $('#form-submit-btn');
-  
-  //Set stripe public key.
-  Stripe.setPublishableKey( $('meta["stripe-key"]').attr('content'));
-  
-  //When user clicks form submit btn
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-signup-btn');
+  //Set Stripe public key.
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  //When user clicks form submit btn,
   submitBtn.click(function(event){
-  //prevent default submission behaviour.
-    event.preventDefault ();
+    //prevent default submission behavior.
+    event.preventDefault();
     submitBtn.val("Processing").prop('disabled', true);
-    
-  //Collect the credit card fields.
-  var ccNum = $('#card_number').val(), 
-      cvcNum = $('#card_code').val(),
-      expMonth = $('#card_month').val(),
-      expYear = $('#card_year').val();
-      
-  //use stripe js library to check for card errors
-  var error = false;
-  
-  //Validate card number.
-  if (!Stripe.card.validateCardNumber(ccNum)) {
-    error = true;
-    alert('The credit card number appears to be invalid');
-  }
-    
-  //Validate CVC 
-  if (!Stripe.card.validateCVC(cvcNum)) {
-    error = true;
-    alert('The CVC number appears to be invalid');
-  }
-    
-  //Validate expiration date
-    if (!Stripe.card.validateExpiry(expMonth,expYear)) {
-    error = true;
-    alert('The expiration date appears to be invalid');
-  }
-    
-  if(error) {
-  //if there are card errors, don't send to stripe
-  submitBtn.prop('disabled', false).val("Sign Up");
-  }
-  
-  else{
-  //Send the card information to stripe.
-    Stripe.createToken({
-    number: ccNum,
-    cvc: cvcNum,
-    exp_month: expMonth,
-    exp_year: expYear
-    }, stripeResponseHandler);
-  }
-
-   return false;
+    //Collect the credit card fields.
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+    //Use Stripe JS library to check for card errors.
+    var error = false;
+    //Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    //Validate CVC number.
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    //Validate expiration date.
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    if (error) {
+      //If there are card errors, don't send to Stripe.
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+      //Send the card info to Stripe.
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
   });
-  
- //stripe will return a card token.
-  function stripeResponseHandler(status, response){
- //get the token from the response
-  var token = response.id; 
- //inject card token as hidden filed into form.
-  theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
-  //submit form to our rails app.
-  theForm.get(0).submit();  
- }
+  //Stripe will return a card token.
+  function stripeResponseHandler(status, response) {
+    //Get the token from the response.
+    var token = response.id;
+    //Inject the card token in a hidden field.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    //Submit form to our Rails app.
+    theForm.get(0).submit();
+  }
 });
