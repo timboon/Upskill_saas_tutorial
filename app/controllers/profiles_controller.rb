9a1e0dd4cb4a757b31f2863dee5a0b02c4e9fbcc
Oliver Wasse <o.wasse@iinet.net.au>
2017-02-16 06:11:14 +0000

    added profiles controller, new view and link on home page

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,8 @@
+class ProfilesController < ApplicationController
+
+# When user makes a GED to /users/:user_id/profile/new
+  def new
+    #When this action is run render a blank profile details form
+  end
+  
+end
