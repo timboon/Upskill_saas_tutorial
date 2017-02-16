c0278d168a1fd3c840ccf3c7ad71bd140ae3ba97
Oliver Wasse <o.wasse@iinet.net.au>
2017-02-16 04:33:06 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-    root to: 'pages#home'
-    devise_for :users, controllers: { registrations: 'users/registrations' }
-    get 'about', to: 'pages#about'
-    resources:contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact'
-    
+  root to: 'pages#home'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do 
+    # use the singular of profile because the user only has one profile 
+    # use plural if the user has many of the object (eg. images)
+    resource :profile
+  end
+  get 'about', to: 'pages#about'
+  resources:contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  
 end
