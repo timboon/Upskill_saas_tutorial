127e0b29f06362e02a7e6a0a65e67f3420795b2e
Oliver Wasse <o.wasse@iinet.net.au>
2017-02-17 04:01:51 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
 # When user makes a GED to /users/:user_id/profile/new
   def new
-    #When this action is run render a blank profile details form
+   @profile = Profile.new
   end
   
 end
