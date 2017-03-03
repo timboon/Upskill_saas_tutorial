47e3198911ebc73e1b0cd133e4616c401ba9fab1
Oliver Wasse <o.wasse@iinet.net.au>
2017-03-03 03:01:26 +0000

    added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,22 @@
 class ProfilesController < ApplicationController
 
-# When user makes a GED to /users/:user_id/profile/new
+# When user makes a GET to /users/:user_id/profile/new
   def new
    @profile = Profile.new
+  end
+  
+  
+# POST request /users/:user_id/profile
+  def create
+    @user = User.find(param[:user_id])
+    @profile = @user.build_profile(profile_params)
+  
+  
+  end
+  
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
   end
   
 end
