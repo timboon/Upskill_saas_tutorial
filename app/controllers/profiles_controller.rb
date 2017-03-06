0db42da6500e6cfcbeb26d327a6944d516acde46
Oliver Wasse <o.wasse@iinet.net.au>
2017-03-06 02:28:27 +0000

    added security and before actions for create and edit actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,11 @@
 class ProfilesController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
 # When user makes a GET to /users/:user_id/profile/new
   def new
    @profile = Profile.new
   end
-  
   
 # POST request /users/:user_id/profile
 def create
@@ -19,6 +20,7 @@ def create
     render action: :new
   end
 end
+
   # GET to /users/:user_id/profile/edit
   def edit
     @user = User.find( params[:user_id])
@@ -42,8 +44,13 @@ end
   end
   
   private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
-  end
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+  
+    def only_current_user
+      @user= User.find(params[:user_id])
+      redirect_to(root_url) unless @user == current_user
+    end
   
 end
