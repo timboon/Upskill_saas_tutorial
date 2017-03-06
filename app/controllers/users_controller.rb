8161861f8b3d8d22b5bc619dbd01c1cdd8688a3c
Oliver Wasse <o.wasse@iinet.net.au>
2017-03-06 03:08:43 +0000

    added users index, view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  
+    # optimise the query to reduce database requests (as opposed to .all) SQL joins
+    @users=User.includes(:profile)
   end
 
   # GET to /users/:id
